2b8f6345a768a609dff38d69e86e0631dfc2f9e5
tonicomer <166739360+tonicomer@users.noreply.github.com>
2025-02-20 15:58:14 -0500

    rewritten get_barycentric_coordinates function

--- barycentric.py
+++ barycentric.py
@@ -5,26 +5,17 @@ import numpy as np
 
 import barycentric
 
-def get_barycentric_coordinates(triangle_coordinates:np.array, point_coordinates:np.ndarray)->np.ndarray:
-    A = triangle_coordinates[:,0]
-    B = triangle_coordinates[:,1]
-    C = triangle_coordinates[:,2]
-    P = point_coordinates
-
-    vertice1 = B - A
-    vertice2 = C - A
-    vertice3 = P - A
-
-    dotv1 = vertice1[0]*vertice1[0] + vertice1[1]*vertice1[1]
-    dotv1and2 = vertice1[0] * vertice2[0] + vertice1[1] * vertice2[1]
-    dotv2 = vertice2[0]*vertice2[0] + vertice2[1] * vertice2[1]
-    dotv1and3 = vertice1[0] * vertice3[0] + vertice1[1]*vertice3[1]
-    dotv2and3 = vertice2[0] * vertice3[0] + vertice2[1]*vertice3[1]
-    scale = dotv1 * dotv2 - dotv1and2 * dotv1and2
-    v = (dotv2*dotv1and3 - dotv1 * dotv2and3) / scale
-    w = (dotv1*dotv2and3 - dotv1 * dotv1and3) / scale
-    u = 1 - v - w
-    return np.array([u, v, w])
+def get_barycentric_coordinates(triangle_coordinates:np.ndarray, point_coordinates:np.ndarray)->np.ndarray:
+    (x1,y1),(x2,y2),(x3,y3)=triangle_coordinates
+    (x,y)=point_coordinates
+    variables=np.array([
+        [x1,x2,x3],
+        [y1,y2,y3],
+        [1,1,1]
+        ]
+    )
+    results=np.array([x,y,1])
+    return np.linalg.solve(variables,results)
 def get_cartesian_coordinates(triangle_coordinates:np.ndarray,barycentric_coordinates:np.ndarray):
     x=triangle_coordinates[0,0]*barycentric_coordinates[0]+triangle_coordinates[0,1]*barycentric_coordinates[1]+triangle_coordinates[0,2]*barycentric_coordinates[2]
     y=triangle_coordinates[1,0]*barycentric_coordinates[0]+triangle_coordinates[1,1]*barycentric_coordinates[1]+triangle_coordinates[1,2]*barycentric_coordinates[2]
