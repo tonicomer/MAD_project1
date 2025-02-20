9b6a15627c3f38ced7d222070d5d96b515bb2635
Santiago Otoya <s.otoya@ufl.edu>
2025-02-20 15:07:43 -0500

    Simpson Function

--- riemann.py
+++ riemann.py
@@ -4,3 +4,7 @@ def left_endpoint(x_vals:np.ndarray,func:np.ufunc):
 def trapezoid(x_vals: np.ndarray, func:np.ufunc)->float:
     answer = np.sum((func(x_vals[:-1]) + func(x_vals[1:]))/2 * (x_vals[1:]-x_vals[:-1]))
     return answer
+def simpson(x_vals: np.ndarray, func: np.ufunc)->float:
+    h = x_vals[1] - x_vals[0]
+    answer = (h/6) * np.sum((func(x_vals[:-1]) + (4*func((x_vals[1:] + x_vals[:-1])/2) + func(x_vals[1:]))))
+    return answer
