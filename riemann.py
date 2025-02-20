cf882b53941c2c04ac28b339055ff286c9efb62a
Russellliao <dongdongliao@ufl.edu>
2025-02-20 14:45:58 -0500

    Update riemann.py

--- riemann.py
+++ riemann.py
@@ -1,4 +1,6 @@
 import numpy as np
+def left_endpoint(x_vals:np.ndarray,func:np.ufunc):
+    return np.sum(func(x_vals[:-1])*(x_vals[1:]-x_vals[:-1]))
 def trapezoid(x_vals: np.ndarray, func:np.ufunc)->float:
     answer = np.sum((func(x_vals[:-1]) + func(x_vals[1:]))/2 * (x_vals[1:]-x_vals[:-1]))
     return answer
